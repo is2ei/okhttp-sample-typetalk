2fab473033547aa9a81dbe71761f6fba3a93d715
Issei Horie <is2ei.horie@gmail.com>
2020-03-29 14:43:31 +0900

    Added build.gradle

---
+++ src/main/java/okhttp3/typetalk/TypetalkClient.java
@@ -0,0 +1,9 @@
+package okhttp3.typetalk;
+
+public final class TypetalkClient {
+    private final TypetalkApi typetalkApi;
+
+    public TypetalkClient(TypetalkApi typetalkApi) {
+        this.typetalkApi = typetalkApi;
+    }
+}
